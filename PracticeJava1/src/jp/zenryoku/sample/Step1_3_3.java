5f42d8a5ea7984ad182ec05a9c1699d422607b47
takk <takk@10.67.239.160>
2018-05-13 11:49:35 +0900

    例外処理の追加

--- PracticeJava1/src/jp/zenryoku/sample/Step1_3_3.java
+++ PracticeJava1/src/jp/zenryoku/sample/Step1_3_3.java
@@ -8,7 +8,15 @@ public class Step1_3_3 {
 		Step1_3_3 step = new Step1_3_3();
 		String input = step.tryTest();
 		System.out.println("あなたの入力自他文字: " + input);
-		
+
+		// 例外を検知しない
+		testPrint();
+		// 例外を検知する
+		try {
+			step.testTrow();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -19,6 +27,7 @@ public class Step1_3_3 {
 	 */
 	public String tryTest() {
 		byte[] b = new byte[5];
+		System.out.println("*** 何か文字を入力してください　***");
 		try {
 			// Systemクラスのstaticメソッド(静的メソッド)
 			System.in.read(b);
@@ -47,5 +56,19 @@ public class Step1_3_3 {
 			// 意図的に投げた例外
 			err.printStackTrace();
 		}
+	}
+
+	public static void testPrint() {
+		System.out.println("例外を検知しないケース");
+		try {
+			throw new Exception("必ず例外を投げます");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testTrow() throws Exception{
+		System.out.println("throws文のメソッド");
+		throw new Exception("throws文からの例外");
 	}
 }
