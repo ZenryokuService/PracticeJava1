e060b870fb576dd2323a42e5cb5df2ce503764cd
takk <yogotak45@gmail.com>
2018-09-21 22:14:19 +0900

    プロパティファイルを参照してコマンドクラス追加

---
+++ PracticeJava1/src/jp/zenryoku/sample/swing/PracSwingMain.java
@@ -0,0 +1,27 @@
+package jp.zenryoku.sample.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PracSwingMain extends JFrame {
+    public static void main(String[] args) {
+        // 土台クラス
+        JFrame frame = new JFrame("FirstSwing");
+        // 土台のコンポーネントを載せる部分(位置などは後で指定する)
+        // コンテナーと呼びます
+        Container con = frame.getContentPane();
+        //コンテナの大記載をしています
+        con.setSize(300, 300);
+        // コンテナの上にラベルを配置します
+        con.add(new JLabel("Hello Swing"));
+        // 土台の上に乗せたものをおきます
+        frame.setContentPane(con);
+        // お約束ごとで閉じる時にこのクラスの起動を終了する設定など。。。
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        // 画面を表示する設定→これをFALSEにすると画面が非表示になる
+        frame.setVisible(true);
+
+
+    }
+}
