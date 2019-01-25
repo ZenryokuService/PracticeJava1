7babcfce4dc1fb2442e12282562fa516d27c1444
takk <yogotak45@gmail.com>
2019-01-25 21:01:05 +0900

    数値１＋数値２＝？の描画クイズ 2019/01/25(出題)-2019/02/01(回答)

--- PracticeJava1/src/jp/zenryoku/fx/RootFxMain.java
+++ PracticeJava1/src/jp/zenryoku/fx/RootFxMain.java
@@ -1,9 +1,13 @@
 package jp.zenryoku.fx;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -26,26 +30,47 @@ public class RootFxMain extends Application {
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// Stageの設定
-		primaryStage.setHeight(VIEW_HEIGHT);
-		primaryStage.setWidth(VIEW_WIDTH);
-		// ラベルの設定
-		Label label = new Label();
-		
-		// ハローワールドを出力する
-		label.setText(myFirstProgram());
-		label.setFont(new Font("RobotRegular", 24));
+public void start(Stage primaryStage) throws Exception {
+	// Stageの設定
+	primaryStage.setHeight(VIEW_HEIGHT);
+	primaryStage.setWidth(VIEW_WIDTH);
+	// レイアウトたて
+	VBox vBox = new VBox(5);
+	// レイアウト横
+	HBox hBox = new HBox(8);
+	// ラベルの設定
+	Label label = new Label();
+	// ハローワールドを出力する
+	label.setText(myFirstProgram());
+	label.setFont(new Font("RobotRegular", 24));
+	vBox.getChildren().add(label);
 
-		// ペインの作成
-		Group root = new Group();
-		root.getChildren().add(label);
-		
-		// シーンの作成
-		Scene scene = new Scene(root, VIEW_WIDTH, VIEW_HEIGHT);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+	// １個目の数値、テキストフィールド
+	TextField text1 = new TextField();
+	text1.setPrefColumnCount(3);
+	text1.setAlignment(Pos.BASELINE_CENTER);
+	hBox.getChildren().add(text1);
+	// 計算式のラベル
+	Label ope = new Label("+");
+	hBox.getChildren().add(ope);
+
+	// ２個目の数値、テキストフィールド
+	TextField text2 = new TextField();
+	text1.setPrefColumnCount(3);
+	text1.setAlignment(Pos.BASELINE_CENTER);
+	hBox.getChildren().add(text2);
+
+	// 縦のレイアウトに追加する
+	vBox.getChildren().add(hBox);
+	// ペインの作成
+	Group root = new Group();
+	root.getChildren().add(vBox);
+
+	// シーンの作成
+	Scene scene = new Scene(root, VIEW_WIDTH, VIEW_HEIGHT);
+	primaryStage.setScene(scene);
+	primaryStage.show();
+}
 
 	/**
 	 * メインメソッド
@@ -63,6 +88,8 @@ public class RootFxMain extends Application {
 	public String myFirstProgram() {
 		// この「hyoji = ""」を「"hyoji = "Hello World"」と修正してください。
 		String hyoji = "Hello World";
+		long num = 12345678901L;
+		float shosu = 123.09876543f;
 		return hyoji;
 	}
 }
