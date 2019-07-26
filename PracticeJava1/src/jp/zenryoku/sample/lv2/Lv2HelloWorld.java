a1a5371810f1718d53d59f086aa1fe4debd482fc
takk <yogotak45@gmail.com>
2019-07-27 00:10:34 +0900

    四則計算処理の実装(バグあり)

--- PracticeJava1/src/jp/zenryoku/sample/lv2/Lv2HelloWorld.java
+++ PracticeJava1/src/jp/zenryoku/sample/lv2/Lv2HelloWorld.java
@@ -1,18 +1,10 @@
-/**
- * Copyright (c) 2019-present Math Kit JavaFX Library All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * Neither the name Math Kit JavaFX Library nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
- */
 package jp.zenryoku.sample.lv2;
 
 import java.util.Scanner;
 
 /**
  * @author takunoji
- *
+ *@see http://zenryokuservice.com/wp/2018/07/20/java-basic-level-1-%E3%80%9Chello-java%E3%80%9C/
  * 2019/07/25
  */
 public class Lv2HelloWorld {
