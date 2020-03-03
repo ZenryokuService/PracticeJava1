def934e55b0b87f573a5323ab70327b501ff389e
takk <yogotak45@gmail.com>
2020-03-03 20:31:18 +0900

    Abstract Factoryパターン

---
+++ PracticeJava1/src/jp/zenryoku/sample/lv2/design/ChildA.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2019-present Math Kit JavaFX Library All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name Math Kit JavaFX Library nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ */
+package jp.zenryoku.sample.lv2.design;
+
+/**
+ * @author takunoji
+ *
+ * 2020/03/03
+ */
+public class ChildA extends Main {
+	public static void main(String[] args) {
+		ChildA main = new ChildA();
+		main.hello();
+	}
+	
+	@Override
+	public String getValue() {
+		return "Hello World";
+	}
+}
