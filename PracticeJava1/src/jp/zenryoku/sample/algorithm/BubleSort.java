9bed8ac5aa52beb11cf6c03d56ae45845d24d6f1
ZenryokuService <tak4545@outlook.jp>
2020-12-01 15:31:05 +0900

    バブルソート実装済み

--- PracticeJava1/src/jp/zenryoku/sample/algorithm/BubleSort.java
+++ PracticeJava1/src/jp/zenryoku/sample/algorithm/BubleSort.java
@@ -2,20 +2,17 @@ package jp.zenryoku.sample.algorithm;
 
 public class BubleSort {
 	public static void main(String[] args) {
-		int[] hako = new int[] {9,8,7,6,5,4,3,2,1};
+		int[] hako = new int[] {6,8,7,9,5,2,3,4,1};
 
-		int tmp = 0;
-		for (int i = 0; i < hako.length; i++) {
-			if (i == 0) {
-				tmp = hako[i];
-				continue;
-			}
-			if (tmp > hako[i]) {
-				tmp = hako[i - 1];
-				int numTemp = hako[i];
-				hako[i - 1] = numTemp;
-				hako[i] = tmp;
-				tmp = numTemp;
+		int target = 0;
+		int arr_size = hako.length - 1;
+		for (int i = 0; i < arr_size; i++) {
+			for (int j = arr_size; j > i; j--) {
+				if (hako[j - 1] < hako[j]) {
+					int numTemp = hako[j - 1];
+					hako[j - 1] = hako[j];
+					hako[j] = numTemp;;
+				}
 			}
 		}
 
